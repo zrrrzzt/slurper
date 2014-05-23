d407bf072e74f74cb4b0d5ae4b336da5685bd4c1
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-23 22:50:46 +0200

    Adds test for valid url

--- getfileurl.js
+++ getfileurl.js
@@ -2,12 +2,17 @@
 
 var request = require('request')
   , cheerio = require('cheerio')
+  , validUrl = require('valid-url')
   ;
 
 module.exports = function getFileUrl(opts, callback){
 
   if(!opts.url){
     return callback(new Error('Missing required param: url'), null);
+  }
+
+  if(opts.url && !validUrl.isWebUri(opts.url)){
+    return callback(new Error('Invalid url'), null);
   }
 
   if(!opts.id){
--- test/testInputs.js
+++ test/testInputs.js
@@ -25,6 +25,25 @@ describe('Slurper - inputs', function(){
 
   });
 
+  it('Should throw if opts.url is not a valid url', function(done){
+
+    var opts = {url:'pysje', id:'#megaman'};
+
+    getFileUrl(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Invalid url/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
   it('Should throw if opts.id is not specified', function(done){
 
     var opts = {url:'http://www.google.com'};
