bf195b87da5f44b93ee463684f2cf04685bb7873
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-23 23:56:07 +0200

    Removes use of --url, adds help

--- index.js
+++ index.js
@@ -2,22 +2,38 @@
 'use strict';
 
 var slurper = require('./slurper')
+  , pkg = require('./package.json')
+  , query = process.argv[2]
   , argv = require('minimist')(process.argv.slice(2))
   , pageUrl = argv.url
   , id = argv.id
-  , filename = argv.filename
-  , version = argv.version;
+  , filename = argv.filename || 'image.gif'
+  ;
 
-if (version){
-  var pkg = require('./package.json');
+function printHelp() {
+  console.log(pkg.description);
+  console.log('');
+  console.log('Usage:');
+  console.log('  $ slurper <url> --id=#id-for-image --filename=filename.gif');
+}
+
+if (!query || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
+  printHelp();
+  return;
+}
+
+if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
   console.log(pkg.version);
   return;
+}
+
+if (query.indexOf('http') !== -1) {
+  pageUrl = argv._[0];
 }
 
 if(pageUrl && id && filename){
   slurper(pageUrl, id, filename);
 } else {
-  console.log('Missing required params.');
-  console.log('Usage:');
-  console.log('slurper --url=http://url.to.page.com --id=#id-to-image --filename=filename.gif');
+  printHelp();
+  return;
 }
