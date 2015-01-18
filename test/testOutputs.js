a525809b601ef91e3501ae433d308a4517006267
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-18 12:39:12 +0100

    Changes code style

--- test/testOutputs.js
+++ test/testOutputs.js
@@ -1,24 +1,30 @@
 'use strict';
 
-var getFileUrl = require('../getfileurl')
-  , assert = require('assert')
-  ;
+var getFileUrl = require('../getfileurl');
+var assert = require('assert');
 
 describe('Slurper - outputs', function(){
 
   it('Should throw if file url is not found', function(done){
 
-    var opts = {url:'https://www.npmjs.org', id:'#megaman'};
+    var opts = {
+      url:'https://www.npmjs.org',
+      id:'#megaman'
+    };
 
     getFileUrl(opts, function(err, data){
       assert.throws(function(){
-          if(err) throw err;
+          if(err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
         }, function(err){
           if((err instanceof Error) && /File url not found/.test(err)){
-            return true
+            return true;
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
@@ -27,7 +33,10 @@ describe('Slurper - outputs', function(){
 
   it('Should return a file url', function(done){
 
-    var opts = {url:'http://www.dagbladet.no/tegneserie/lunch/', id:'#lunch-stripe'};
+    var opts = {
+      url:'http://www.dagbladet.no/tegneserie/lunch/',
+      id:'#lunch-stripe'
+    };
 
     getFileUrl(opts, function(err, data){
       if(err){
