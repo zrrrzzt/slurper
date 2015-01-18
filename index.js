a6c8baabdd6364ace9b857bfc4d21d994e64b95f
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-18 15:28:37 +0100

    Adds downloadFile as separate module

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var request = require('request');
 var getImageUrl = require('./lib/getImageUrl');
 var getPageData = require('./lib/getPageData');
 
-function saveFile(fileUrl, fileName) {
-  request(fileUrl).pipe(fs.createWriteStream(fileName));
-}
-
 function doSlurp(options, callback){
   if(!options){
     return callback(new Error('Missing required input: options'), null);
